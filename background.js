5b9a532d934d09c1ccd03a08a25dab87ff53bcee
Jason <jason1996429@gmail.com>
2020-03-02 00:42:53 -0500

    slight clean up

--- background.js
+++ background.js
@@ -12,17 +12,12 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
                             `Changing "referer" on ${url} header value from ${header.value} to nothing`
                         );
                         header.value = '';
+                        console.log(`${details.requestHeaders.referer}`);
                     }
                 }
             }
         }
 
-        // This is a normal array and can be modified like any array (use `push` to add new entries, for example)
-        // let requestHeaders = details.requestHeaders;
-
-        // Uncomment the below to log details:
-        // console.log(`onBeforeSendHeaders: Request id ${details.requestId}: "${details.method} ${details.url}" (type: ${details.type})`);
-        console.log(`${details.requestHeaders.referer}`);
         return {requestHeaders: details.requestHeaders};
     },
 
@@ -30,8 +25,5 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
         urls: ['<all_urls>']
     },
 
-    // "Extra Info" Spec: "blocking" says that this callback might modify (change headers) the request,
-    // "requestHeaders" makes sure that `details` includes the request headers, and "extraHeaders" makes it so
-    // the details includes some extra (more privacy-sensitive) headers that would normally not be provided by Chrome
     ['requestHeaders', 'extraHeaders', 'blocking']
 );
